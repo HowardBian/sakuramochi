ef423c3ca3f62c7596e852cb67c395aaee45a859
HowardBian <howardbian623@gmail.com>
2026-02-23 21:17:14 -0800

    feat: artists, cafe, schedule, stage pages are now under construction

--- app/cafe/page.tsx
+++ app/cafe/page.tsx
@@ -1,49 +1,52 @@
 "use client";
-import { CafeFaq } from "@/components/cafeFaq";
-import { easeInOut, motion } from "framer-motion";
+// import { CafeFaq } from "@/components/cafeFaq";
+// import { easeInOut, motion } from "framer-motion";
+
+import ComingSoon from "../coming-soon/page";
 
 export default function Cafe() {
-  return (
-    <main>
-      <div className="relative flex flex-col w-full min-h-dvl items-center p-10 pb-24 text-black text-pretty">
-        <div
-          data-aos="fade-up"
-          className="relative flex flex-row xl:w-2/3 md:justify-items-start justify-between"
-        >
-          <div className="flex flex-col z-40 items-center md:items-start gap-2 p-0">
-            <p className="font-sans uppercase text-2xl font-bold text-pink tracking-wider">
-              Pop-Up Hanami 2025
-            </p>
-            <p className="hidden sm:block font-display-en uppercase text-black text-9xl">
-              Cosplay Cafe
-            </p>
-            <p className="sm:hidden font-display-en uppercase text-black text-8xl">
-              Sakura Cosplay Cafe
-            </p>
-          </div>
-        </div>
-        <div data-aos="fade-up" className="lg:w-2/3">
-          <p className="font-sans font-medium text-lg">
-          {`Step into a world of pastel dreams at this year's Sakura Cosplay Cafe, where the magic of the early 2000s Shoujo comes to life! Sip on a Sailor Moonblast served by waiters in your favorite Shoujo-inspired outfits, and who knows? A little spark of romance might just find its way into your cup~`}
-          </p>
-        </div>
-        <motion.img
-          src="/menu.png"
-          className="md:block float rounded-xl z-0 p-10"
-          initial={{ y: 500, rotateZ: 50 }}
-          animate={{ y: 0, rotateZ: 0 }}
-          transition={{
-            duration: 2,
-            bounce: 0.25,
-            type: "spring",
-            ease: "easeInOut",
-          }}
-        ></motion.img>
-        <div data-aos="fade-up" className="lg:w-2/3 my-24">
-          <CafeFaq />
-        </div>
+  return <ComingSoon />;
+  // return (
+  //   <main>
+  //     <div className="relative flex flex-col w-full min-h-dvl items-center p-10 pb-24 text-black text-pretty">
+  //       <div
+  //         data-aos="fade-up"
+  //         className="relative flex flex-row xl:w-2/3 md:justify-items-start justify-between"
+  //       >
+  //         <div className="flex flex-col z-40 items-center md:items-start gap-2 p-0">
+  //           <p className="font-sans uppercase text-2xl font-bold text-pink tracking-wider">
+  //             Pop-Up Hanami 2025
+  //           </p>
+  //           <p className="hidden sm:block font-display-en uppercase text-black text-9xl">
+  //             Cosplay Cafe
+  //           </p>
+  //           <p className="sm:hidden font-display-en uppercase text-black text-8xl">
+  //             Sakura Cosplay Cafe
+  //           </p>
+  //         </div>
+  //       </div>
+  //       <div data-aos="fade-up" className="lg:w-2/3">
+  //         <p className="font-sans font-medium text-lg">
+  //         {`Step into a world of pastel dreams at this year's Sakura Cosplay Cafe, where the magic of the early 2000s Shoujo comes to life! Sip on a Sailor Moonblast served by waiters in your favorite Shoujo-inspired outfits, and who knows? A little spark of romance might just find its way into your cup~`}
+  //         </p>
+  //       </div>
+  //       <motion.img
+  //         src="/menu.png"
+  //         className="md:block float rounded-xl z-0 p-10"
+  //         initial={{ y: 500, rotateZ: 50 }}
+  //         animate={{ y: 0, rotateZ: 0 }}
+  //         transition={{
+  //           duration: 2,
+  //           bounce: 0.25,
+  //           type: "spring",
+  //           ease: "easeInOut",
+  //         }}
+  //       ></motion.img>
+  //       <div data-aos="fade-up" className="lg:w-2/3 my-24">
+  //         <CafeFaq />
+  //       </div>
         
-      </div>
-    </main>
-  );
+  //     </div>
+  //   </main>
+  // );
 }
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-export default function Schedule() {
-  return (
-    <main className="flex flex-col w-full min-h-screen p-10 pb-24 text-black text-pretty">
-      <p data-aos="fade-up" className="w-full max-w-5xl font-display-en uppercase text-black text-6xl md:text-7xl lg:text-8xl mb-6 md:text-nowrap">
-        Schedule & Activities
-      </p>
+import ComingSoon from "../coming-soon/page";
 
-          <p data-aos="fade-up" className="font-sans font-medium text-lg">
-          {`Evening events:`}<br/>
-          {`- `}<b>{`Idol Showcase`}</b>{` - Norm Theatre - START: 7:00 PM`}<br/>
-          {`- `}<b>{`Animirai After Party`}</b>{` - Abdul Ladha Building - START: 8:30 PM (19+ only)`}
-          </p>
-      <div data-aos="fade-up" className="w-full bg-gray-100 p-6 flex flex-col items-center">
-        <div className="flex flex-col gap-4 w-full lg:w-2/3 px-2 md:px-4">
-          <img src="full_schedudle.png" className="rounded-lg shadow-md w-full" alt="Full Schedule" />
-          <img src="panels_description_p1.png" className="rounded-lg shadow-md w-full" alt="Panels Description Part 1" />
-          <img src="panels_description_p2.png" className="rounded-lg shadow-md w-full" alt="Panels Description Part 2" />
-        </div>
-      </div>
-    </main>
-  );
+export default function Schedule() {
+  return <ComingSoon />;
+  // return (
+  //   <main className="flex flex-col w-full min-h-screen p-10 pb-24 text-black text-pretty">
+  //     <p data-aos="fade-up" className="w-full max-w-5xl font-display-en uppercase text-black text-6xl md:text-7xl lg:text-8xl mb-6 md:text-nowrap">
+  //       Schedule & Activities
+  //     </p>
+
+  //         <p data-aos="fade-up" className="font-sans font-medium text-lg">
+  //         {`Evening events:`}<br/>
+  //         {`- `}<b>{`Idol Showcase`}</b>{` - Norm Theatre - START: 7:00 PM`}<br/>
+  //         {`- `}<b>{`Animirai After Party`}</b>{` - Abdul Ladha Building - START: 8:30 PM (19+ only)`}
+  //         </p>
+  //     <div data-aos="fade-up" className="w-full bg-gray-100 p-6 flex flex-col items-center">
+  //       <div className="flex flex-col gap-4 w-full lg:w-2/3 px-2 md:px-4">
+  //         <img src="full_schedudle.png" className="rounded-lg shadow-md w-full" alt="Full Schedule" />
+  //         <img src="panels_description_p1.png" className="rounded-lg shadow-md w-full" alt="Panels Description Part 1" />
+  //         <img src="panels_description_p2.png" className="rounded-lg shadow-md w-full" alt="Panels Description Part 2" />
+  //       </div>
+  //     </div>
+  //   </main>
+  // );
 }
