91a5340e52233d3a29db14765cfb9904678e8f99
HowardBian <howardbian623@gmail.com>
2026-03-25 15:36:38 -0700

    feat: updated the artist lineup and floorplans to 2026 version

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -16,55 +16,116 @@ export default function Volunteer() {
           </div>
         </div>
 
-        {/* <div 
+        <div 
           data-aos="fade-up"
-          className="flex flex-col z-40 items-center md:items-start gap-2 p-4 w-3/4">
-          <img src="Artist_Alley_Floorplan.png"></img>          
-        </div> */}
+          className="flex flex-col z-40 items-center md:items-start gap-2 p-4 w-full md:w-3/4">
+          <Image
+            src="/GreatHallFloorplan2026.webp"
+            alt="Great Hall floor plan"
+            width={4096}
+            height={2720}
+            className="w-full h-auto"
+          />
+          <Image
+            src="/Room2306And2301Floorplan2026.webp"
+            alt="Room 2306 and Room 2301 floor plan"
+            width={4096}
+            height={2170}
+            className="w-full h-auto"
+          />
+        </div>
           
         <div
-          data-aos="fade-up"
           className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 p-4 gap-6 w-full max-w-6xl"
         >
           <Image
             className="w-full h-auto"
-            src="/frame_1.webp"
-            alt="Artist feature frame 1"
+            src="/A1.webp"
+            alt="Artist Section A 1st frame"
             width={978}
             height={928}
           />
           <Image
             className="w-full h-auto"
-            src="/frame_2.webp"
-            alt="Artist feature frame 2"
+            src="/A2.webp"
+            alt="Artist Section A 2nd frame"
             width={978}
             height={928}
           />
           <Image
             className="w-full h-auto"
-            src="/frame_3.webp"
-            alt="Artist feature frame 3"
+            src="/A3.webp"
+            alt="Artist Section A 3rd frame"
             width={978}
             height={928}
           />
           <Image
             className="w-full h-auto"
-            src="/frame_4.webp"
-            alt="Artist feature frame 4"
+            src="/A4.webp"
+            alt="Artist Section A 4th frame"
             width={978}
             height={928}
           />
           <Image
             className="w-full h-auto"
-            src="/frame_5.webp"
-            alt="Artist feature frame 5"
+            src="/A5.webp"
+            alt="Artist Section A 5th frame"
             width={978}
             height={928}
           />
           <Image
             className="w-full h-auto"
-            src="/frame_6.webp"
-            alt="Artist feature frame 6"
+            src="/A6.webp"
+            alt="Artist Section A 6nd frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/A7.webp"
+            alt="Artist Section A 7th frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/A8-B1.webp"
+            alt="Artist Section A 1st frame to Artist Section B 1st frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/B2.webp"
+            alt="Artist Section B 2nd frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/B3.webp"
+            alt="Artist Section B 3rd frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/C1.webp"
+            alt="Artist Section C 1st frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/C2.webp"
+            alt="Artist Section C 2nd frame"
+            width={977}
+            height={652}
+          />
+          <Image
+            className="w-full h-auto"
+            src="/C3.webp"
+            alt="Artist Section C 3rd frame"
             width={977}
             height={652}
           />
